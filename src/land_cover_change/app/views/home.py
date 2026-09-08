6329a37ffebd7a93b48ec82b6f52e1d43c4c5ab7
Obinna <okeyobinna2001@gmail.com>
2026-09-08 09:17:46 +0100

    feat: streamlit web pages

---
+++ src/land_cover_change/app/views/home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Home page")
+st.write(
+    "Welcome to the Home page of Urban Land Cover Change: Uteh/Iwogban and Environs, Benin City (2016–2025). " \
+    "It's going to contain the project title, a short introduction, study area and analysis period, Data source, key findings" \
+    "or summary statistics and link or navigation to the interactive map, area comparison and transition analysis"
+)
---
+++ src/land_cover_change/app/views/transition_analysis.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Transition Analysis")
+st.write("This is going to contain the transition analysis of the different land cover class across the years")
